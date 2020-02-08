17c3a3ce9a503dd0e6bae348abb194aa7ec4a5a8
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-07 21:08:33 -0800

    create cube

--- report.py
+++ report.py
@@ -7,18 +7,24 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+f = open('score.txt','r')
+
 e = Entity(model=Terrain('height_map', skip=8), texture='explosion', scale=500, scale_y=500)
 Sky(rotation_y=125)
 
-help_text = Text(text='Controls:\n\nw: Forward\ns: Back\na: Left\nd: Right\ne: Up\nq: Down\n', origin=(-.5,.5), position=window.top_left*.95, color=color.black)
+Entity(model = 'cube', texture = 'white_cube', color = color.blue, highlight_color = color.lime, scale=100, scale_y=100)
+
+score = f.read()
+
+help_text = Text(text=f'Controls:\n\nw: Forward\ns: Back\na: Left\nd: Right\ne: Up\nq: Down\nScore: {score}', origin=(-.5,.5), position=window.top_left*.95, color=color.black)
 
 scene.fog_color = color.gray
-scene.fog_density = .01
+scene.fog_density = .005
 
 fpc = FirstPersonController(speed=50)
 
 window.exit_button.visible = True
-window.fps_counter.enabled = False
+window.fps_counter.enabled = True
 mouse.visible = False
 
 camera.shader = camera_vertical_blur_shader
