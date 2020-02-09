7da8389a08c49bfba4b3ca8c36a9328d857521c8
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-08 23:16:52 -0800

    add program installed

---
+++ engine.py
@@ -0,0 +1,20 @@
+import os
+import pwd
+import re
+import socket
+import subprocess
+import sys
+
+score = 0
+points = []
+
+
+def programInstalled(program):
+   p = subprocess.Popen("dpkg -l | grep " +program, shell=True, stdout=subprocess.PIPE)
+   d = p.stdout.read()
+   p.stdout.close()
+   p.wait()
+   if d:
+       return True
+   else:
+       return False
