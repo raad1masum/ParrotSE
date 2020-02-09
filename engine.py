ca80bd9b26b4cfd2f422708f82c2f831a1c75e8c
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-08 23:23:58 -0800

    add user check

--- engine.py
+++ engine.py
@@ -8,6 +8,16 @@ import sys
 score = 0
 points = []
 
+def userCheck(user):
+   global score
+   person = 0
+   for line in open('/etc/passwd'):
+       if user in line:
+           person = 1
+   if person == 0:
+       score = score+1
+       points.append('Removed The User '+user)
+
 def updates(topic,repo):
    global score
    p = subprocess.Popen("cat /etc/apt/sources.list", shell=True, stdout=subprocess.PIPE)
