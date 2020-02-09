ed52ecfb11f1904ee4841ccb0110796d70a209f0
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-08 23:25:39 -0800

    add malware check

--- engine.py
+++ engine.py
@@ -47,3 +47,9 @@ def packageRemoved(package):
        return True
    else:
        return False
+
+def malwareCheck(file_path):
+   global score
+   if not os.path.isfile(file_path):
+      score = score+1
+      points.append('Removed Harmful File')
