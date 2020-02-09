97d9efdf56edf9b5164487a08c4d6994801c871c
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2020-02-08 23:18:06 -0800

    add program removed

--- engine.py
+++ engine.py
@@ -18,3 +18,13 @@ def programInstalled(program):
        return True
    else:
        return False
+
+def programRemoved(program):
+   p = subprocess.Popen("dpkg -l | grep " +program, shell=True, stdout=subprocess.PIPE)
+   d = p.stdout.read()
+   p.stdout.close()
+   p.wait()
+   if not d:
+       return True
+   else:
+       return False
