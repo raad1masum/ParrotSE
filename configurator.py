d3b905bd0588698e5b12546ca5ff3351cf3ad370
raad1masum <piraadwan@gmail.com>
2020-02-10 17:58:12 -0800

    move test to configurator

--- configurator.py
+++ configurator.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+from engine import *
+
+pointsTotal = 0
+
+def gainPoints(points):
+	pointsTotal = pointsTotal + points
+
+if checkUpdate():
+	gainPoints(3)
--- test.py
+++
@@ -1,9 +0,0 @@
-from engine import *
-
-pointsTotal = 0
-
-def gainPoints(points):
-	pointsTotal = pointsTotal + points
-
-if checkUpdate():
-	gainPoints(3)
