cf561ae4b3d0ab43a11ee4a1245749d56bb6df1d
root <Raadwan Masum>
2020-02-23 14:53:14 -0800

    create command

--- init.sh
+++ init.sh
@@ -8,5 +8,18 @@ apt-get -y install python3.6
 apt-get -y install python3-pip
 apt-get -y install python3-setuptools
 
+echo '* * * * * root /usr/local/bin/score' >> /etc/crontab
+echo 'ALL ALL=NOPASSWD: /usr/local/bin/score' > /etc/sudoers.d/ScoreEngine
+chattr +i /etc/sudoers.d/ScoreEngine
+echo "/usr/lib/notify-osd/notify-osd &" >> /etc/profile
+
+echo "
+#!/bin/bash
+python3 ParrotSE.py" > /usr/local/bin/score
+
+chown root:root /usr/local/bin/score
+chmod 4755 /usr/local/bin/score
+chattr +i /usr/local/bin/score
+
 cp report.desktop /home/*/Desktop
 chmod 777 /home/*/Desktop/report.desktop
