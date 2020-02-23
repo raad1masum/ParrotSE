92e78ae09a237b1a9a893bc2900534bcd4b2388e
raad1masum <piraadwan@gmail.com>
2020-02-23 14:08:46 -0800

    add init desktop icon

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ apt-get -y update
 apt-get -y install python3.6
 apt-get -y install python3-pip
 apt-get -y install python3-setuptools
+
+cp report.desktop /home/*/Desktop
+chmod 777 /home/*/Desktop/report.desktop
